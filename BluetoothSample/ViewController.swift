210d9b3aef11606eb367c7a43927472dc8c7cbea
Togami Yuki <togaminnnn@gmail.com>
2018-09-22 11:07:58 +0800

    first commit タッチした点の座標を取り出す処理。

--- BluetoothSample/ViewController.swift
+++ BluetoothSample/ViewController.swift
@@ -10,14 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+    
+    //タッチした時の位置を取得する。
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! //このタッチイベントの場合確実に1つ以上タッチ点があるので`!`つけても可能。
+        let location = touch.location(in: self.view) //in: には対象となるビューを入れる。
+        print("x座標：",location.x,",y座標：",location.y)//タッチした点の座標を表示。
+        
+        
+        
+        
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
